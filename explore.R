5ddfd2d999545fe9dfce6dc10097bccd1248661d
Silvia Bordogna <silviabord@users.noreply.github.com>
2018-07-02 22:58:50 +0100

    aggiungi simulazione

--- explore.R
+++ explore.R
@@ -62,24 +62,24 @@ patients$leave_date_time =
 
 
 #modifico leave date di modo che se uno dovrebbe uscire dopo le 18 lo postpongo alle 8 del giorno dopo
-patients$adjust_1 = ymd_hms(paste("1991-10-01 ", 
-                                  hour(patients$leave_date_time), ":",
-                                  minute(patients$leave_date_time),":00", sep=""))
-patients$adjust_late_afternoon = difftime(ymd_hms(paste("1991-10-02 ", "08:00:00", sep="")),
-                                          patients$adjust_1, units = "mins")
-
-patients$adjust_early_morning = difftime(ymd_hms(paste("1991-10-01 ", "08:00:00", sep="")),
-                                         patients$adjust_1, units="mins")
-
-patients$leave_date_time_old = patients$leave_date_time
-
-filter = hour(patients$leave_date_time) %in% c(18:23)
-patients[filter,"leave_date_time"] = patients[filter,"leave_date_time"]+
-  minutes(patients[filter,"adjust_late_afternoon"])
-
-filter = hour(patients$leave_date_time) %in% c(00:06)
-patients[filter,"leave_date_time"] = patients[filter,"leave_date_time"]+
-  minutes(patients[filter,"adjust_early_morning"])
+# patients$adjust_1 = ymd_hms(paste("1991-10-01 ", 
+#                                   hour(patients$leave_date_time), ":",
+#                                   minute(patients$leave_date_time),":00", sep=""))
+# patients$adjust_late_afternoon = difftime(ymd_hms(paste("1991-10-02 ", "08:00:00", sep="")),
+#                                           patients$adjust_1, units = "mins")
+# 
+# patients$adjust_early_morning = difftime(ymd_hms(paste("1991-10-01 ", "08:00:00", sep="")),
+#                                          patients$adjust_1, units="mins")
+# 
+# patients$leave_date_time_old = patients$leave_date_time
+# 
+# filter = hour(patients$leave_date_time) %in% c(18:23)
+# patients[filter,"leave_date_time"] = patients[filter,"leave_date_time"]+
+#   minutes(patients[filter,"adjust_late_afternoon"])
+# 
+# filter = hour(patients$leave_date_time) %in% c(00:06)
+# patients[filter,"leave_date_time"] = patients[filter,"leave_date_time"]+
+#   minutes(patients[filter,"adjust_early_morning"])
 
 
 #View(head(patients[,c("arrival_date_time","leave_date_time",
@@ -230,71 +230,64 @@ mean(patients$Length_of_Stay_day)
 
 
 
-##############################################################
-############      stima pazienti oraria           ############
-##############################################################
+######################################################################################          
+############     check se con los stimato si replicano dati historical           #####
+######################################################################################          
 
-hour_series = seq(ymd_hms('2015-01-02 00:00:00'), ymd_hms('2015-10-30 23:00:00'), by = '30 min')  
-final = data.frame(date_time = hour_series)
-
-#write.table(patients, "C:\\Users\\Giacomo Monti\\Desktop\\decision model\\final projects\\patients_modified.csv", 
-#            sep = ";", quote = FALSE, row.names = F)
-
-timestamp()
-for (i in seq_along(final$date_time)){
-  j = final$date_time[i]
-  
-  final[i,"n_patient_im_in"] = 
-    length(patients[patients$arrival_date_time <= j & patients$leave_date_time > j &
-                      patients$Surgery_Type == "Internal_Medicine" &
-                      patients$Section == "8000595","Patient_ID"])
-  
-  final[i,"n_patient_im_out"] = 
-    length(patients[patients$arrival_date_time <= j & patients$leave_date_time > j &
-                      patients$Surgery_Type == "Internal_Medicine" &
-                      patients$Section == "other","Patient_ID"])
-  
-  final[i,"n_patient_other_in"] = 
-    length(patients[patients$arrival_date_time <= j & patients$leave_date_time > j &
-                      patients$Surgery_Type == "Others" &
-                      patients$Section == "8000595","Patient_ID"])
-  
-  final[i,"n_patient_other_out"] = 
-    length(patients[patients$arrival_date_time <= j & patients$leave_date_time > j &
-                      patients$Surgery_Type == "Others" &
-                      patients$Section == "other","Patient_ID"])
-  print(i)
-}
-
-###################   controllo se i dati tornano con historical   ###################
-plot(final$n_patient_im_in+final$n_patient_other_in, type="l")
-abline(h = 19, col=2)
-
-check = final %>% filter(hour(final$date_time)==00)
-check$date = ymd(floor_date(check$date_time, unit = c("day")))
-check$recovery = check$n_patient_im_in
-#+ check$n_patient_other_in
-#+check$n_patient_other_in
-check_2= merge(check,  historical_info, by.x = "date", by.y = "Date")
-
-#errore stima con historical
-sqrt(sum((check_2$patients-check_2$recovery)^2)/dim(check_2)[1])
-#sum(abs(check_2$recovery-check_2$patients))/dim(check_2)[1]
-
-#plot(historical_info$patients, type="l")
-plot(check$recovery, type="l", lwd = 1, lambda=0.9)
-abline(h = 19, col=2)
-abline(h = mean(check$recovery))
-lines(historical_info$patients,col="blue",type="l")
-abline(h = mean(historical_info$patients) ,col="blue")
-legend(0, 27, legend=c("Estimate", "Historical"),
-       col=c("black", "blue"), lty=1, cex=0.6, bty="n")
+# hour_series = seq(ymd_hms('2015-01-02 00:00:00'), ymd_hms('2015-10-30 23:00:00'), by = '30 min')  
+# final = data.frame(date_time = hour_series)
 # 
-# final$tot_recovery = final$n_patient_im_in + final$n_patient_other_in
-# final$tot_patient = final$n_patient_im_in + final$n_patient_other_in+
-# final$n_patient_im_out + final$n_patient_other_out
 # 
-# hist(final$tot_patient)
+# timestamp()
+# for (i in seq_along(final$date_time)){
+#   j = final$date_time[i]
+#   
+#   final[i,"n_patient_im_in"] = 
+#     length(patients[patients$arrival_date_time <= j & patients$leave_date_time > j &
+#                       patients$Surgery_Type == "Internal_Medicine" &
+#                       patients$Section == "8000595","Patient_ID"])
+#   
+#   final[i,"n_patient_im_out"] = 
+#     length(patients[patients$arrival_date_time <= j & patients$leave_date_time > j &
+#                       patients$Surgery_Type == "Internal_Medicine" &
+#                       patients$Section == "other","Patient_ID"])
+#   
+#   final[i,"n_patient_other_in"] = 
+#     length(patients[patients$arrival_date_time <= j & patients$leave_date_time > j &
+#                       patients$Surgery_Type == "Others" &
+#                       patients$Section == "8000595","Patient_ID"])
+#   
+#   final[i,"n_patient_other_out"] = 
+#     length(patients[patients$arrival_date_time <= j & patients$leave_date_time > j &
+#                       patients$Surgery_Type == "Others" &
+#                       patients$Section == "other","Patient_ID"])
+#   print(i)
+# }
+# 
+# ###################   controllo se i dati tornano con historical   ###################
+# plot(final$n_patient_im_in+final$n_patient_other_in, type="l")
+# abline(h = 19, col=2)
+# 
+# check = final %>% filter(hour(final$date_time)==00)
+# check$date = ymd(floor_date(check$date_time, unit = c("day")))
+# check$recovery = check$n_patient_im_in
+# #+ check$n_patient_other_in
+# #+check$n_patient_other_in
+# check_2= merge(check,  historical_info, by.x = "date", by.y = "Date")
+# 
+# #errore stima con historical
+# sqrt(sum((check_2$patients-check_2$recovery)^2)/dim(check_2)[1])
+# #sum(abs(check_2$recovery-check_2$patients))/dim(check_2)[1]
+# 
+# #plot(historical_info$patients, type="l")
+# plot(check$recovery, type="l", lwd = 1, lambda=0.9)
+# abline(h = 19, col=2)
+# abline(h = mean(check$recovery))
+# lines(historical_info$patients,col="blue",type="l")
+# abline(h = mean(historical_info$patients) ,col="blue")
+# legend(0, 27, legend=c("Estimate", "Historical"),
+#        col=c("black", "blue"), lty=1, cex=0.6, bty="n")
+# 
 
 
 #################################
@@ -344,7 +337,7 @@ simulate_arrival=function(start = "2018-01-01 00:05:00", n_month=1,
     
     #stime 
     rexp_interarrival = round(rexp(1,lambda_interarrivi),0)
-    rexp_surgery = round(rexp(1,lambda_surgery),0)
+    rexp_surgery = max(50, round(rexp(1,lambda_surgery),0))
     rexp_los = round(rexp(1,lambda_los),0)
     
     #simulate series
@@ -360,113 +353,170 @@ simulate_arrival=function(start = "2018-01-01 00:05:00", n_month=1,
   }
   return(simulation) }
 
-simulation = simulate_arrival(start = "2018-02-01 00:05:00", n_month=1,
+simulation = simulate_arrival(start = "2018-01-01 00:05:00", n_month=2,
                               lambda_interarrivi, lambda_surgery, lambda_los)
 
 ####################
 ###   function   ###
 ####################
-queue_ordered = simulation[order(simulation$start_waiting),]
-simulation_full = simulation
+simulation = simulation [simulation$arrival_date_time > ymd_hms("2018-01-15 00:00:00"), ]
 
-recovery = data.frame(
-  patient_id = integer(),
-  start_waiting = integer(),
-  admission = integer(),
-  leave = integer()
-)
-
-class(recovery$start_waiting) <- "POSIXct"
-class(recovery$admission) <- "POSIXct"
-class(recovery$leave) <- "POSIXct"
-
-
-n_letti = 19
-date = min(simulation$start_waiting)
-
-while (date < max(simulation$start_waiting)){
-  #     max(simulation$start_waiting)){
-  #       ymd_hms("2018-02-04 20:57:00")){
+simulate_recovery = function(
+  data = simulation, 
+  n_letti = 19,
+  max_attesa = 60,  
+  each=15#minuti
+){
   
+  #ordino la coda
+  queue_ordered = simulation[order(simulation$start_waiting),]
+  simulation_full = simulation
   
+  #inizializzo df
+  recovery = data.frame(
+    patient_id = integer(),
+    start_waiting = integer(),
+    admission = integer(),
+    leave = integer()
+  )
   
+  class(recovery$start_waiting) <- "POSIXct"
+  class(recovery$admission) <- "POSIXct"
+  class(recovery$leave) <- "POSIXct"
   
+  #inizio ciclo da quando inizia a essere pronto ad entrare in recovery
+  date = min(simulation$start_waiting)
   
-  #quanti letti si liberano dalla recovery
-  free_bed = n_letti - (dim(recovery)[1]-sum(recovery$leave < date))
-  
-  if (free_bed != 0) {
+  while (date < max(simulation$start_waiting)+days(7)){
     
-    #segno in simulation full dati uscita recovery
-    patient_out = recovery[recovery$leave < date,c("patient_id","admission","leave")]
-    simulation_full[simulation_full$patient_id %in% patient_out$patient_id,"admission"] = patient_out$admission
-    simulation_full[simulation_full$patient_id %in% patient_out$patient_id,"leave"] = patient_out$leave
-    #tolgo dalla recovery quelli usciti
-    recovery = recovery[!recovery$patient_id %in% patient_out$patient_id,]
+    #quanti letti si liberano dalla recovery se non si libera nessuno passo ad ora dopo
+    free_bed = n_letti - (dim(recovery)[1]-sum(recovery$leave < date))
     
-    #prendo i primi della queue pronti (se ci sono) e li metto in recovery
-    ready = queue_ordered[queue_ordered$start_waiting<=date,]
-    
-    if (dim(ready)[1]!=0){
-      #print(free_bed)
-      free_bed = if( dim(ready)[1] < free_bed){dim(ready)[1]}else{free_bed} 
-      #print(free_bed)
+    #------------------ 
+    if (free_bed != 0) {
       
-      recovery= rbind(
-        recovery,
+      #segno in simulation full dati uscita recovery
+      patient_out = recovery[recovery$leave < date,c("patient_id","admission","leave")]
+      simulation_full[simulation_full$patient_id %in% patient_out$patient_id,"admission"] = patient_out$admission
+      simulation_full[simulation_full$patient_id %in% patient_out$patient_id,"leave"] = patient_out$leave
+      
+      #tolgo dalla recovery quelli usciti
+      recovery = recovery[!recovery$patient_id %in% patient_out$patient_id,]
+      
+      #prendo i primi della queue pronti (se ci sono) e li metto in recovery
+      ready = queue_ordered[queue_ordered$start_waiting<=date,]
+      
+      #-------------
+      if (dim(ready)[1]!=0){
         
-        data.frame(
-          patient_id = ready[seq(1 :  free_bed),"patient_id"],
-          start_waiting = ready[seq(1 :  free_bed),"start_waiting"],
-          admission = rep(date, free_bed),
-          leave = date + minutes(ready[seq(1 :  free_bed),"los"])
-        ))
+        free_bed = if( dim(ready)[1] < free_bed){dim(ready)[1]}else{free_bed} 
+        
+        recovery= rbind(
+          recovery,
+          
+          data.frame(
+            patient_id = ready[seq(1 :  free_bed),"patient_id"],
+            start_waiting = ready[seq(1 :  free_bed),"start_waiting"],
+            admission = rep(date, free_bed),
+            leave = date + minutes(ready[seq(1 :  free_bed),"los"])
+          ))
+        
+        #rimuovo dalla coda quelli che possono entrare
+        queue_ordered = queue_ordered[- seq(1 :  free_bed),]
+      }
+      #-------------  
       
-      #rimuovo dalla coda quelli che possono entrare
+    }
+    
+    
+    #sistemata la recovery controllo se ce qualcuno che aspetta da troppo
+    ready2 = queue_ordered[queue_ordered$start_waiting<=date,]
+    attesa = difftime(date , ready2$start_waiting,  units = "mins")
+    
+    
+    #-------------  
+    if(any(attesa>=max_attesa)){
       
-      #print(dim(queue_ordered)[1])
-      queue_ordered = queue_ordered[- seq(1 :  free_bed),]
-      #print(c(free_bed, dim(queue_ordered)[1]))
-    }}
-  
-  #sistemata la recovery controllo se ce qualcuno che aspetta da troppo
-  ready2 = queue_ordered[queue_ordered$start_waiting<=date,]
-  attesa = difftime(date , ready2$start_waiting,  units = "mins")
-  
-  #print(any(attesa>60))
-  if(any(attesa>=60)){
+      trasferimenti = ready2[attesa>=max_attesa,]
+      #sistemare se ci sono più di 1 paziente per match giusto
+      simulation_full[simulation_full$patient_id %in% trasferimenti$patient_id,"admission"] = date
+      simulation_full[simulation_full$patient_id %in% trasferimenti$patient_id,"leave"] =
+        date + minutes(trasferimenti$los)
+      simulation_full[simulation_full$patient_id %in% trasferimenti$patient_id,"transfer"] = TRUE
+      
+      queue_ordered =  queue_ordered[!queue_ordered$patient_id %in% trasferimenti$patient_id,]
+    }
+    #-------------  
     
-    trasferimenti = ready2[attesa>60,]
-    
-    simulation_full[simulation_full$patient_id %in% trasferimenti$patient_id,"admission"] = date
-    simulation_full[simulation_full$patient_id %in% trasferimenti$patient_id,"leave"] =
-      date + minutes(trasferimenti$los)
-    simulation_full[simulation_full$patient_id %in% trasferimenti$patient_id,"transfer"] = TRUE
-    
-    queue_ordered =  queue_ordered[!queue_ordered$patient_id %in% trasferimenti$patient_id,]
-  }
+    date = date + minutes(each)
+  } #....fine while
   
-  date = date + minutes(30)
-  #  print(date)
-}#-----------------fine while
+  return(simulation_full)}#---fine funzione
+
+simulation_full = simulate_recovery(data = simulation,  n_letti = 19, max_attesa = 60,  each=15)
 
 class(simulation_full$leave) <- "POSIXct"
 class(simulation_full$admission) <- "POSIXct"
 
+simulation_full$transfer = ifelse(is.na(simulation_full$transfer), FALSE, simulation_full$transfer)
+
 simulation_full$waiting_time = difftime(simulation_full$admission , simulation_full$start_waiting,
                                         units = "mins")
+simulation_full = simulation_full [simulation_full$arrival_date_time >= ymd_hms("2018-02-01 00:00:00"), ]
+
+
+#primi risultati
+mean_waiting = mean(simulation_full$waiting_time)
+max_waiting = max(simulation_full$waiting_time)
+trasferiti = sum(simulation_full$transfer, na.rm=T)
+max(temp$max_letti)
 
 
 
 
+##############################################################
+############      stima pazienti oraria           ############
+##############################################################
+
+hour_series = seq(ymd_hms('2018-02-01 00:00:00'), ymd_hms('2018-03-01 23:00:00'), by = '30 min')  
+final = data.frame(date_time = hour_series)
+
+
+timestamp()
+for (i in seq_along(final$date_time)){
+  j = final$date_time[i]
+  
+  final[i,"n_patient_im_in"] = 
+    length(simulation_full[simulation_full$admission <= j & simulation_full$leave > j &
+                             simulation_full$transfer == FALSE ,"patient_id"])
+  
+  final[i,"n_patient_im_out"] = 
+    length(simulation_full[simulation_full$admission <= j & simulation_full$leave > j &
+                             simulation_full$transfer == TRUE ,"patient_id"])
+  
+  final[i,"mean_waiting"] = 
+    quantile(simulation_full[simulation_full$admission <= j & simulation_full$leave > j &
+                               simulation_full$transfer == FALSE ,"waiting_time"], 0.5)
+  
+  final[i,"p90_waiting"] = 
+    quantile(simulation_full[simulation_full$admission <= j & simulation_full$leave > j &
+                               simulation_full$transfer == FALSE ,"waiting_time"], 0.9)
+  print(i)
+}
 
 
 
+#risultati 2
+b=19
+u=1 #emergenze
+a = sum(b - final$n_patient_im_in  > u, na.rm=T) #quante mezzore con più di u letti vuoti (Male)
+b = sum(final$p90_waiting  > 60, na.rm=T) #quanti mezzore con più di 60 minuti d attesa 90 perc
+c = sum(final$n_patient_im_out == 1 , na.rm=T) #quante mezzore abbiamo occupato almeno 1 letto fuori
+d = sum(final$n_patient_im_out == 2 , na.rm=T) #letti mezzora -- moltiplico per 2
+e = sum(final$n_patient_im_out == 3 , na.rm=T)
+f = sum(final$n_patient_im_out == 4 , na.rm=T)
 
-
-
-
-
+min(a+b+c+d+e+f)
 
 ######################################
 #     score e funzione  obiettivo   #
